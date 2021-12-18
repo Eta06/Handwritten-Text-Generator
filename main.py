1c1e35580903a2121d9fa75759a4e01fb7532d35
Eta06 <emrtnhalim@gmail.com>
2021-12-18 21:13:12 +0300

    main.py file comments added.

--- main.py
+++ main.py
@@ -2,3 +2,4 @@
 import pywhatkit
 #   We specify the handwritten text in the file we want to be created.
 pywhatkit.text_to_handwriting("Love is the most beautiful feeling in life. But losing the person you love is the saddest thing to experience.", rgb=(203, 85, 237))
+#   Also you can change the text color with setting up the rgb() part in the code.
