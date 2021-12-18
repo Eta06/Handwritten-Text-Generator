ffa5e8747d2a79c94df4a8a7ea41cc2d162eaf97
Eta06 <emrtnhalim@gmail.com>
2021-12-18 21:08:15 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+#   Importing required libraries
+import pywhatkit
+#   We specify the handwritten text in the file we want to be created.
+pywhatkit.text_to_handwriting("Love is the most beautiful feeling in life. But losing the person you love is the saddest thing to experience.", rgb=(203, 85, 237))
